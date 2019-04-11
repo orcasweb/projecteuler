838b895c2687c5d4521cf68a823d0da625b6614c
orcasweb <user@unknown>
2019-04-11 09:14:19 -0400

    solved two ways - using custom and built in functions

---
+++ source/Problem_020.py
@@ -0,0 +1,34 @@
+'''
+Created on Apr 11, 2019
+
+@author: orcasweb
+'''
+import string
+from math import factorial
+factNum = 100
+finalResult = 0
+
+def getFact(n):
+    if(0<n<101):
+        if(n==1):
+            return 1
+        else:
+            return n*getFact(n-1)
+    else:
+        return 1
+
+def getSumIntList(intList):
+    global finalResult
+    for item in intList:
+        finalResult = finalResult + item
+    return finalResult
+# get the factorial of the number
+factN = getFact(factNum)
+print(factN)
+# convert the factorial to string
+# then using map function convert each char in string to int list
+# pass the list to the function getSumIntList
+print(getSumIntList(list(map(int,str(factN)))))
+
+# ANOTHER WAY TO SOLVE USING THE BUILTIN FUNCTIONS
+print(sum(list(map(int,str(factorial(factNum))))))
